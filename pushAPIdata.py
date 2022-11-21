e2af7f82fdbdee057a31bc58cbe715a373274cec
Sai Shibu N B <saishibu38380@gmail.com>
2022-11-21 14:12:27 +0530

    Update pushAPIdata.py

--- pushAPIdata.py
+++ pushAPIdata.py
@@ -2,7 +2,6 @@ import pymysql #pip3 install pymysql
 import requests
 
 #create a mysql connection
-conn =pymysql.connect(database="weather_data_schema",user="landslide",password="amma@123",host="localhost")
 
 OPEN_WEATHER_MAP_API_KEY = "b0ca473301875d2a23b6634f62749c3c"
 
@@ -25,6 +24,9 @@ cur=conn.cursor()
 #data = {"sl no":2,"id":2015,"DISTRICT":"KODAGU","TALUKNAME":"MADIKERI","HOBLINAME":"GALIBIDU","Day1":0.0}
 
 #cur.execute("INSERT INTO dailyData(sl no,id, DISTRICT, TALUKNAME, HOBLINAME, Day1) VALUES(%(2)s,%(2015)s,%("KODAGU")s,%("MADIKERI")s,%("GALIBIDU")s,%(0.0)s);",data)
+
+conn =pymysql.connect(database="weather_data_schema",user="landslide",password="amma@123",host="localhost")
+
 
 cur.execute("INSERT INTO `weather_data` (`latitude`, `longitude`, `temp`, `wspd`, `wgust`, 'pressure') VALUES (%(lat)s,%(lon)s,%(temp)s,%(wind_speed)s,%(wind_gust)s,%(pressure)s);",apiData)
 
