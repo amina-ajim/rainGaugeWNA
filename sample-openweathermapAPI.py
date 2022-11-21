56a3f38f82bdc579f53e127fe437454dc148d8b0
Sai Shibu N B <saishibu38380@gmail.com>
2022-11-21 11:36:21 +0530

    Update sample-openweathermapAPI.py

--- sample-openweathermapAPI.py
+++ sample-openweathermapAPI.py
@@ -6,7 +6,7 @@ x=10.054
 y=77.120
 
 exclude = "current,alerts,minutely"
-api_url = "https://api.openweathermap.org/data/2.5/onecall?lat="+str(x)+"&lon="+str(y)+"&exclude="+exclude+"&appid="+{OPEN_WEATHER_MAP_API_KEY}+"&units=metric"
+api_url = "https://api.openweathermap.org/data/2.5/onecall?lat="+str(x)+"&lon="+str(y)+"&appid=b0ca473301875d2a23b6634f62749c3c&units=metric"
 
 response = requests.get(api_url)
 print(response)
