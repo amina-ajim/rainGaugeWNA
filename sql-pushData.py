1543b156c7edcd3755ed378c62310272d3b806e2
Amina Ajim <64067242+amina-ajim@users.noreply.github.com>
2022-11-21 11:08:37 +0530

    Update sql-pushData.py

--- sql-pushData.py
+++ sql-pushData.py
@@ -11,7 +11,7 @@ data = {"sl no":2,"id":2015,"DISTRICT":"KODAGU","TALUKNAME":"MADIKERI","HOBLINAM
 
 #cur.execute("INSERT INTO dailyData(sl no,id, DISTRICT, TALUKNAME, HOBLINAME, Day1) VALUES(%(2)s,%(2015)s,%("KODAGU")s,%("MADIKERI")s,%("GALIBIDU")s,%(0.0)s);",data)
 
-cur.execute("INSERT INTO `dailyData` (`sl no`, `id`, `DISTRICT`, `TALUKNAME`, `HOBLINAME`, `Day1`) VALUES (%("s1 no")s,%("id")s,%("DISTRICT")s,%("TALUKNAME")s,%("HOBLINAME")s,%("Day1")s);",data)
+cur.execute("INSERT INTO `dailyData` (`sl no`, `id`, `DISTRICT`, `TALUKNAME`, `HOBLINAME`, `Day1`) VALUES (%(s1 no)s,%(id)s,%(DISTRICT)s,%(TALUKNAME)s,%(HOBLINAME)s,%(Day1)s);",data)
 
 conn.commit()
 conn.close()
