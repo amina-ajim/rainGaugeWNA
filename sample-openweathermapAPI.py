1a63761c031e6b95748e36ae2f6e6ae0ca4dee80
Amina Ajim <64067242+amina-ajim@users.noreply.github.com>
2022-11-21 11:28:52 +0530

    Update sample-openweathermapAPI.py

--- sample-openweathermapAPI.py
+++ sample-openweathermapAPI.py
@@ -1,9 +1,9 @@
 import requests
 
-OPEN_WEATHER_MAP_API_KEY = "your key here"
+OPEN_WEATHER_MAP_API_KEY = "b0ca473301875d2a23b6634f62749c3c"
 
-x=yourlat (9.052)
-y=yourlong (72.369)
+x=yourlat (10.054)
+y=yourlong (77.120)
 
 exclude = "current,alerts,minutely"
 api_url = "https://api.openweathermap.org/data/2.5/onecall?lat="+str(x)+"&lon="+str(y)+"&exclude="+exclude+"&appid="+{OPEN_WEATHER_MAP_API_KEY}+"&units=metric"
