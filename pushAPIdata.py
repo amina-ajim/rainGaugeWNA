2961cb15ece04349f2d4da6de5b6112d285610c5
Amina Ajim <64067242+amina-ajim@users.noreply.github.com>
2022-11-21 12:25:36 +0530

    Update pushAPIdata.py

--- pushAPIdata.py
+++ pushAPIdata.py
@@ -12,6 +12,7 @@ y=77.120
 api_url = "https://api.openweathermap.org/data/2.5/onecall?lat="+str(x)+"&lon="+str(y)+"&appid=b0ca473301875d2a23b6634f62749c3c&units=metric&exclude=hourly,daily,alerts,minutely"
 
 response = requests.get(api_url)
+data = response.content
 print(response.content)
 
 #create a mysql cursor
@@ -21,7 +22,7 @@ cur=conn.cursor()
 
 #cur.execute("INSERT INTO dailyData(sl no,id, DISTRICT, TALUKNAME, HOBLINAME, Day1) VALUES(%(2)s,%(2015)s,%("KODAGU")s,%("MADIKERI")s,%("GALIBIDU")s,%(0.0)s);",data)
 
-cur.execute("INSERT INTO `weather_data` (`latitude`, `longitude`, `temp`, `wspd`, ``, `Day1`) VALUES (%(sl no)s,%(id)s,%(DISTRICT)s,%(TALUKNAME)s,%(HOBLINAME)s,%(Day1)s);",data)
+cur.execute("INSERT INTO `weather_data` (`latitude`, `longitude`, `temp`, `wspd`, `wgust`, 'pressure') VALUES (%(lat)s,%(lon)s,%(temp)s,%(wind_speed)s,%(wind_gust)s,%(pressure)s);",data)
 
 conn.commit()
 conn.close()
