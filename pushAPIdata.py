75051abeca518fb866960e54a4ab3f4dcb220c0a
Sai Shibu N B <saishibu38380@gmail.com>
2022-11-21 14:10:27 +0530

    Update pushAPIdata.py

--- pushAPIdata.py
+++ pushAPIdata.py
@@ -12,7 +12,7 @@ y=77.120
 api_url = "https://api.openweathermap.org/data/2.5/onecall?lat="+str(x)+"&lon="+str(y)+"&appid=b0ca473301875d2a23b6634f62749c3c&units=metric&exclude=hourly,daily,alerts,minutely"
 
 response = requests.get(api_url)
-data = response.content
+data = response.content.decode()
 print(type(data))
 current =  data["current"]
 
